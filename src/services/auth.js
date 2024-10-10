4929e36a73446fc93a4d4099315c3715405fd6e5
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-10-10 16:59:52 +0300

    change mentor server and create my own

--- src/services/auth.js
+++ src/services/auth.js
@@ -157,7 +157,7 @@ export const resetPassword = async (payload) => {
 	try {
 		entries = jwt.verify(payload.token, env('JWT_SECRET'));
 	} catch (err) {
-		if (err instanceof Error) throw createHttpError(401, err.message);
+		if (err instanceof Error) throw createHttpError(401, 'Token is expired or invalid.');
 		throw err;
 	}
 
